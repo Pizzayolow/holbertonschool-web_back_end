908770ac3db8343b6544991860864302ad32a0e9
Pizzayolow <ralliard.valentin@gmail.com>
2023-08-28 10:03:30 +0200

    str concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""concatenate"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenate two strings and return the concatened one"""
+    strcpy = str1 + str2
+    return strcpy
