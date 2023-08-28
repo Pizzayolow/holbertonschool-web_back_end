9f5648f6588eb15b0f65f207a2eae758f17bae54
Pizzayolow <ralliard.valentin@gmail.com>
2023-08-28 10:45:27 +0200

    add comment

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,4 +4,5 @@ from typing import Union, List, Dict, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """return a tuple str and a squared of int or float"""
     return(k, v*v)
