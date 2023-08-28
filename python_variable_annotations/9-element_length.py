d565b09fea485e38692814f36f7b1f8ab21a0a39
Pizzayolow <ralliard.valentin@gmail.com>
2023-08-28 11:21:57 +0200

    fix name function

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -3,6 +3,6 @@
 from typing import Union, List, Dict, Tuple, Callable, Sequence, Iterable
 
 
-def element_lenght(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """duck from an iterable"""
     return [(i, len(i)) for i in lst]
