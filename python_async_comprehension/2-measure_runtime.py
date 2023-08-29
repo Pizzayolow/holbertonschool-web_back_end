fecb1efd5585ecf198443742c5e3228f74752908
Pizzayolow <ralliard.valentin@gmail.com>
2023-08-29 10:57:11 +0200

    measure time task 3

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""measure time between gathers"""
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure time"""
+    list = []
+    n = 4
+    start_time = time.time()
+    coros = [async_comprehension() for i in range(n)]
+    await asyncio.gather(*coros)
+    end_time = time.time()
+    total_time = end_time - start_time
+    return (total_time)
