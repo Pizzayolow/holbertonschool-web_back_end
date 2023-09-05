1aeca4f784d5f74f20bad71fd36cfaffc3185c6e
Pizzayolow <ralliard.valentin@gmail.com>
2023-09-05 15:15:01 +0200

    Tuple and not tuple

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,7 +3,7 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     '''return a tuple with start and end size'''
     end_index = page * page_size
     start_index = end_index - page_size
