1b69e0fe98de9362cbeb44ee8485e0dc1b3f78fe
pizzayolow <ralliard.valentin@gmail.com>
2023-09-05 02:25:20 -0700

    task 12, all stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,39 +3,42 @@
 
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
-db_name = "logs"
-col_name = "nginx"
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    school_collection = client.my_db.school
 
-db = client[db_name]
-collection = db[col_name]
+    db_name = "logs"
+    col_name = "nginx"
 
-document_count = collection.count_documents({})
-print(f"{document_count} logs")
+    db = client[db_name]
+    collection = db[col_name]
 
-filter_get = {"method": "GET"}
-get_count = collection.count_documents(filter_get)
+    document_count = collection.count_documents({})
+    print(f"{document_count} logs")
 
-filter_put = {"method": "PUT"}
-put_count = collection.count_documents(filter_put)
+    filter_get = {"method": "GET"}
+    get_count = collection.count_documents(filter_get)
 
-filter_post = {"method": "POST"}
-post_count = collection.count_documents(filter_post)
+    filter_put = {"method": "PUT"}
+    put_count = collection.count_documents(filter_put)
 
-filter_patch = {"method": "PATCH"}
-patch_count = collection.count_documents(filter_patch)
+    filter_post = {"method": "POST"}
+    post_count = collection.count_documents(filter_post)
 
-filter_delete = {"method": "DELETE"}
-delete_count = collection.count_documents(filter_delete)
+    filter_patch = {"method": "PATCH"}
+    patch_count = collection.count_documents(filter_patch)
 
-filter_status = {"path": "/status", "method": "GET"}
-status_count = collection.count_documents(filter_status)
+    filter_delete = {"method": "DELETE"}
+    delete_count = collection.count_documents(filter_delete)
 
-print("Methods:")
-print(f"\tmethod GET: {get_count}")
-print(f"\tmethod POST: {post_count}")
-print(f"\tmethod PUT: {put_count}")
-print(f"\tmethod PATCH: {patch_count}")
-print(f"\tmethod DELETE: {delete_count}")
-print(f"{status_count} status check")
+    filter_status = {"path": "/status", "method": "GET"}
+    status_count = collection.count_documents(filter_status)
+
+    print("Methods:")
+    print(f"\tmethod GET: {get_count}")
+    print(f"\tmethod POST: {post_count}")
+    print(f"\tmethod PUT: {put_count}")
+    print(f"\tmethod PATCH: {patch_count}")
+    print(f"\tmethod DELETE: {delete_count}")
+    print(f"{status_count} status check")
 
