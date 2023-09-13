f42bb9515b695c5dc895c5a9f2232c03235cd24d
Pizzayolow <ralliard.valentin@gmail.com>
2023-09-13 17:12:15 +0200

    task 5 error from ihnerit class

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (new.target === Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  static evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
