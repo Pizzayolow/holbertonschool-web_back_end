de6f17e64a0c842c2e6cc35485d3d296972e7822
Pizzayolow <ralliard.valentin@gmail.com>
2023-10-02 15:42:47 +0200

    add a return for the Promise.all

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
   const promises = [photo, user];
-  Promise.all(promises)
+  return Promise.all(promises)
     .then((result) => {
       console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
