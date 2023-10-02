ddfefb9edeff19d463d148cb6cafdd55a41056c3
Pizzayolow <ralliard.valentin@gmail.com>
2023-10-02 15:25:16 +0200

    task 3, promise.all

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
+  const promises = [photo, user];
+  Promise.all(promises)
+    .then((result) => {
+      console.log(result[0].body, result[1].firstName, result[1].lastName);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
