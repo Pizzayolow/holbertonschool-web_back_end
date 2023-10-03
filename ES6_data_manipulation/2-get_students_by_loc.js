debbc41357ed441ead1ffc23a14825b523c27d90
Pizzayolow <ralliard.valentin@gmail.com>
2023-10-03 11:26:08 +0200

    remove the verification if its an array

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,4 @@
 export default function getStudentsByLocation(getListStudents, city) {
-  if (getListStudents instanceof Array) {
-    const filterCity = getListStudents.filter((find) => find.location.includes(city));
-    return filterCity;
-  }
-  return console.log('Il faut un tableau');
+  const filterCity = getListStudents.filter((find) => find.location.includes(city));
+  return filterCity;
 }
