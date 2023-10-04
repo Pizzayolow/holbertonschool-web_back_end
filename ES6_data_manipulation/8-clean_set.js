00b780def342720ab38c8cc4bff2c63f5113afad
Pizzayolow <ralliard.valentin@gmail.com>
2023-10-04 10:09:23 +0200

    compare, concat, filter and return

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,19 @@
+export default function cleanSet(inputSet, startString) {
+  const emptyStr = '';
+  if (startString !== '') {
+    // Convert the Set to an array for easier manipulation
+    const setArray = Array.from(inputSet);
+
+    // Use the filter() method to keep only the values that start with startString
+    const filteredArray = setArray.filter((value) => value.startsWith(startString));
+
+    // Use the map() method to extract the part of the string after startString
+    const cleanedArray = filteredArray.map((value) => value.substring(startString.length));
+
+    // Join the cleanedArray elements with "-"
+    const resultString = cleanedArray.join('-');
+
+    return resultString;
+  }
+  return emptyStr;
+}
