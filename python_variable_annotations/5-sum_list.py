7c834ffc93d650f976e3ce4888d13ad39301aca6
Valentin RALLIARD <vralliard@ccld.com>
2024-07-30 15:21:20 +0200

    add typing library

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """sum of list in annotation"""
+from typing import List
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List[float]) -> float:
     """sum of list in annotation"""
     total: int = 0
     for num in input_list:
