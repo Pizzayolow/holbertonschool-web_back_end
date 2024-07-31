786e76f81ab13118a8cd359131269727d876c0ad
Valentin RALLIARD <vralliard@ccld.com>
2024-07-31 11:02:16 +0200

    add Tuple by typing library

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import Union
+from typing import Union, Tuple
 """to kv with annotations"""
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """to kv with annotations"""
     return (k, v*v)
