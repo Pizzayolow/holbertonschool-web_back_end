986583918838cef65a65762f13486b368a234932
Valentin RALLIARD <vralliard@ccld.com>
2024-09-23 12:01:18 +0200

    add comments

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,7 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
+    """A function used to obfuscate the log message"""
     for field in fields:
         message = re.sub(fr'{field}=.+?{separator}',
                          f'{field}={redaction}{separator}',
