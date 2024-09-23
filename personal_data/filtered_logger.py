b0386af7010070f99d0fbd469f557fda6593c400
Valentin RALLIARD <vralliard@ccld.com>
2024-09-23 12:07:40 +0200

    checker, always

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,11 +2,8 @@
 """Regex-ing module to obfuscate log messages"""
 import re
 
-
 def filter_datum(fields, redaction, message, separator):
     """A function used to obfuscate the log message"""
     for field in fields:
-        message = re.sub(fr'{field}=.+?{separator}',
-                         f'{field}={redaction}{separator}',
-                         message)
+        message = re.sub(fr'{field}=.+?{separator}', f'{field}={redaction}{separator}', message)
     return message
