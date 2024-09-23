60a33ed8dcb7f08f333ace9dd79c0adb0f5c5be3
Valentin RALLIARD <vralliard@ccld.com>
2024-09-23 12:08:44 +0200

    idk

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 """Regex-ing module to obfuscate log messages"""
 import re
+from typing import List
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     """A function used to obfuscate the log message"""
     for field in fields:
-        message = re.sub(fr'{field}=.+?{separator}', f'{field}={redaction}{separator}', message)
+        message = re.sub(fr'{field}=.+?{separator}',
+                         f'{field}={redaction}{separator}',
+                         message)
     return message
